aad026dd29b92fec18c8ca410e0a2703135882b3
Hossein <94081715+Hosseinm13@users.noreply.github.com>
2025-05-16 22:22:04 +0330

    Delete ticket_sales directory

--- ticket_sales/concerts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Concert, Ticket
-
-admin.site.register(Concert)
-admin.site.register(Ticket)
--- ticket_sales/static/js/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Add any dynamic functionality here
-    console.log('Site loaded successfully!');
-});
