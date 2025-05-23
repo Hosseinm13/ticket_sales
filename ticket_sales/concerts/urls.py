fe7e7f87fd8d16848421662d7ba33cf086ae0af9
Hossein <94081715+Hosseinm13@users.noreply.github.com>
2025-05-23 19:42:26 +0330

    Delete ticket_sales directory

--- ticket_sales/concerts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('concert/<int:concert_id>/', views.concert_detail, name='concert_detail'),
-    path('cart/', views.cart, name='cart'),
-    path('checkout/', views.checkout, name='checkout'),
-]
